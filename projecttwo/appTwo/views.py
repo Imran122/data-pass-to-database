45f7a4f5b45605b7965662c43a4b10c1ade21e17
imran122 <imransardar122@gmail.com>
2020-03-22 10:58:31 +0600

    data pass to database

---
+++ projecttwo/appTwo/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from appTwo.models import User
+from . import forms
+# Create your views here.
+
+def index(request):
+    return render(request, 'appTwo/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users': user_list}
+    return render(request, 'appTwo/users.html', context=user_dict)
+
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print("validation success")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
+
+    return render(request, 'appTwo/form_page.html', {'form':form})
